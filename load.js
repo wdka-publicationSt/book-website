a97fc2d033236716b5a6712d556182b82a837388
Castro0o <andre@andrecastro.info>
2017-05-03 13:21:24 +0200

    seperated js from html

--- load.js
+++ load.js
@@ -1,50 +1,50 @@
+
+function create_sections(content){
+    // wraps <h1> & elments that follow, until next <h1>, within a <div class="section" id="section_N'>...  
+    var sections = [];
+    h1s = content.find('h1');	    
+    h1s.each( function(h1){
+    	var el_id = $(this).attr('id'); //get its id		    
+    	var h1_next = $(this).nextUntil('h1'); //els from h1 to next h1
+    	h1_next = h1_next.add($(this)); //include h1
+    	sections.push(h1_next);
+    });    
+    $('div#content').text('');
+    var sections_length = sections.length;
+    for (var i=0; i < sections_length; i++) {   
+	$('div#content').append('<div class="section" id="section_'+i +'" ></div>');
+	$('div#section_'+i).append( sections[i] );
+    }    
+    return [sections, h1s]
+}
+
 function generate_toc(headings){
-    console.log('menu');
-    var menu = '' //an empty menu to start
+    var menu = '' // start w/ empty menu
     headings.each( function(h){ //for each heading
 	var el_id = $(this).attr('id'); //get its id
 	var el_text = $(this).text(); //get its text
 	var menu_item = '<li><a href="#' + el_id + '">' + el_text + '</a></li>' //build a <li> with <a> pointing to each <h1> in text 
 	menu = menu + menu_item;
-    })
-	menu = '<ul>'+menu+'</ul>'; //wrap all <li>s inside <ul> </ul>
+    });	
+    menu = '<ul>'+menu+'</ul>'; //wrap all <li>s inside <ul> </ul>
+    $('div#content').before(menu); //insert menu before div#content
     return menu
 }
 
-var sections = [];
-var h1s ;
+
 $(document).ready(
     function(){
-	// maybe content should not be loaded onto the DOM
-	// but to a variable that can be parsed
+	// can content not be loaded into the DOM
+	// but to a variable that can be parsed?
 	$.get('./content.html', function(content){
 	    html = $.parseHTML( content );
 	    $('div#content').append( content );
-//	    cont = content; 
-	    h1s = $('div#content').find('h1');
-	    
-	    h1s.each( function(h1){
-		var el_id = $(this).attr('id'); //get its id		    
-		//wrap chpters into divs
-		var h1_next = $(this).nextUntil('h1'); //els from h1 to next h1
-		h1_next = h1_next.add($(this)); //include h1
-		sections.push(h1_next);
-	    })
-		console.log( sections );
-	    $('div#content').text('');
-	    sections_length = sections.length;
-	    for (var i=0; i < sections_length; i++) {   
-		$('div#content').append('<div class="section" id="section_'+i +'" ></div>');
-		$('div#section_'+i).append( sections[i] );
-	    }
-	    } , 'html' )
+	    var sections_h1s= create_sections( $('div#content') );
+	    var sections = sections_h1s[0];
+	    var h1s = sections_h1s[1];
+	    console.log( sections, h1s  );
+	    var menu = generate_toc(h1s); //create menu by running generate_toc(all_h1s) 
+	} , 'html' )
 	
-	
-	////// Creating menu
-	$('h1').css('color','#000');
-	var menu = generate_toc(h1s); //create menu by running generate_toc(all_h1s) 
-	$('div#content').before(menu); //insert menu before div#content
-
     }
 )
-
